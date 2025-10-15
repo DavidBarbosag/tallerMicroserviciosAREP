d086bd18d720089ef7514097a42c4da9fd7a7d9d
unknown <david.barbosa-g@labinfo.is.escuelaing.edu.co>
2025-10-15 11:25:34 -0500

    resultados

---
+++ README.md
@@ -0,0 +1,18 @@
+# Resultados Taller
+
+
+## Square
+
+![img.png](assets/SquareTest.png)
+
+## API Gateway Square
+
+![resApiGateway](assets/apiGateRes.png)
+
+## Login
+
+![loginRes](assets/loginRes.png)
+
+## API Gateway Login Test
+
+![img.png](assets/loginGateTest.png)
---
+++ src/main/java/edu/eci/arep/SecurityUtils.java
@@ -0,0 +1,11 @@
+package edu.eci.arep;
+
+public class SecurityUtils {
+
+    public static User login(User u){
+        System.out.println("Username: " + u.getUsername());
+        System.out.println("Password: " + u.getPassword());
+        u.setPassword("");
+        return u;
+    }
+}
